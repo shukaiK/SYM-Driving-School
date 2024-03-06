39989a578ac798bd48a6ea9d2718309c896d2ebe
Monir Fathalla <monir.fathalla@hotmail.com>
2024-03-05 21:13:55 -0800

    new login and student form, edited usercontroller

--- focusdriving/src/main/java/com/cmpt/focusdriving/controllers/UsersController.java
+++ focusdriving/src/main/java/com/cmpt/focusdriving/controllers/UsersController.java
@@ -25,57 +25,49 @@ public class UsersController {
     @Autowired
     private UserRepository usersRepository;
 
-    public UsersController() {
-        
-        // users.add(new Users("John", "password", 30));
-        // users.add(new Users("Jane", "password", 25));
-        // users.add(new Users("Doe", "password", 35));
-
+    @GetMapping("/users/all")
+    public String getAllUsers(Model model) {
+    System.out.println("Hello from all users");
+    List<Users> users = usersRepository.findAll(); // db
+    model.addAttribute("users", users);
+    return "users/all";
     }
 
-    // @GetMapping("/users/all")
-    // public String getAllUsers(Model model) {
-    // System.out.println("Hello from all users");
-    // List<Users> users = usersRepository.findAll(); // db
-    // model.addAttribute("users", users);
-    // return "users/all";
-    // }
+    @GetMapping("/login")
+    public String getLogin(Model model, HttpServletRequest request, HttpSession
+    session){
+    Users user = (Users) session.getAttribute("session_user");
+    if (user == null){
+    return "users/login";
+    }
+    else {
+    model.addAttribute("user",user);
+    return "users/protected";
+    }
+    }
 
-    // @GetMapping("/login")
-    // public String getLogin(Model model, HttpServletRequest request, HttpSession
-    // session){
-    // Users user = (Users) session.getAttribute("session_user");
-    // if (user == null){
-    // return "users/login";
-    // }
-    // else {
-    // model.addAttribute("user",user);
-    // return "users/protected";
-    // }
-    // }
+    @PostMapping("/login")
+    public String login(@RequestParam Map<String,String> formData, Model model,
+    HttpServletRequest request, HttpSession session){
+    // processing login
+    String name = formData.get("name");
+    String pwd = formData.get("password");
+    List<Users> userlist = usersRepository.findByNameAndPassword(name, pwd);
+    if (userlist.isEmpty()){
+    return "users/login";
+    }
+    else {
+    // success
+    Users user = userlist.get(0);
+    request.getSession().setAttribute("session_user", user);
+    model.addAttribute("user", user);
+    return "users/protected";
+    }
+    }
 
-    // @PostMapping("/login")
-    // public String login(@RequestParam Map<String,String> formData, Model model,
-    // HttpServletRequest request, HttpSession session){
-    // // processing login
-    // String name = formData.get("name");
-    // String pwd = formData.get("password");
-    // List<Users> userlist = usersRepository.findByNameAndPassword(name, pwd);
-    // if (userlist.isEmpty()){
-    // return "users/login";
-    // }
-    // else {
-    // // success
-    // Users user = userlist.get(0);
-    // request.getSession().setAttribute("session_user", user);
-    // model.addAttribute("user", user);
-    // return "users/protected";
-    // }
-    // }
-
-    // @GetMapping("/logout")
-    // public String destroySession(HttpServletRequest request){
-    // request.getSession().invalidate();
-    // return "/users/login";
-    // }
+    @GetMapping("/logout")
+    public String destroySession(HttpServletRequest request){
+    request.getSession().invalidate();
+    return "/users/login";
+    }
 }
