caa979bb892f838805a415389f4ea8c01c9b5dea
Sloane <git@sloaney.ca>
2024-03-05 19:40:17 -0800

    Created Maven project

---
+++ focusdriving/src/main/java/com/cmpt/focusdriving/FocusdrivingApplication.java
@@ -0,0 +1,13 @@
+package com.cmpt.focusdriving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusdrivingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusdrivingApplication.class, args);
+	}
+
+}
---
+++ focusdriving/src/test/java/com/cmpt/focusdriving/FocusdrivingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpt.focusdriving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusdrivingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
