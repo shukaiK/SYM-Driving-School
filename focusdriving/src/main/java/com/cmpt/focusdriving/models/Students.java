9cb1ba7b23854a399f242e1b6afe7eec68a14781
Monir Fathalla <monir.fathalla@hotmail.com>
2024-03-08 11:24:06 -0800

    studentrepo

--- focusdriving/src/main/java/com/cmpt/focusdriving/models/Students.java
+++ focusdriving/src/main/java/com/cmpt/focusdriving/models/Students.java
@@ -1,5 +1,72 @@
 package com.cmpt.focusdriving.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
 public class Students {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int sid;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String requestMessage;
+    
+
+    
+    public Students() {
+    }
+
+    public Students(int sid, String name, String email, String phoneNumber, String requestMessage) {
+        this.sid = sid;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.requestMessage=requestMessage;
+    }
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRequestMessage() {
+        return requestMessage;
+    }
+
+    public void setRequestMessage(String requestMessage) {
+        this.requestMessage = requestMessage;
+    }
 
 }
+
+
+    
