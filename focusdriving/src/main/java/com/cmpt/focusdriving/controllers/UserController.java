51945b9cc66b670489a281acae016ca187ae180b
ShukaiG <shukai.workmail@gmail.com>
2024-03-12 23:21:50 -0700

    database

--- focusdriving/src/main/java/com/cmpt/focusdriving/controllers/UserController.java
+++ focusdriving/src/main/java/com/cmpt/focusdriving/controllers/UserController.java
@@ -87,11 +87,8 @@ public class UserController {
     }
 
     @GetMapping("/user/logout")
-public String destroySession(HttpServletRequest request) {
-    HttpSession session = request.getSession(false); // Get existing session if it exists
-    if (session != null) {
-        session.invalidate();
-    }
-    return "redirect:/user/login"; // Redirect to login page
+    public String destroySession(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "/user/login";
     }
 }
