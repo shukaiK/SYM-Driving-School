c4214bb90121516451576c68d84aeeda66dbb580
Monir Fathalla <monir.fathalla@hotmail.com>
2024-05-19 21:41:28 -0700

    a

--- focusdriving/src/main/java/com/cmpt/focusdriving/FocusdrivingApplication.java
+++ focusdriving/src/main/java/com/cmpt/focusdriving/FocusdrivingApplication.java
@@ -3,15 +3,11 @@ package com.cmpt.focusdriving;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class FocusdrivingApplication {
 
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().directory("/etc/secrets/.env").load();
-        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
-        System.setProperty("DEFAULT_PASS", dotenv.get("DEFAULT_PASS"));
 		SpringApplication.run(FocusdrivingApplication.class, args);
 	}
 
