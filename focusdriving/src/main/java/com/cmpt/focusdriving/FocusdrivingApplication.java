1fae91a322d73a2c67ad78d2e888393184f1359f
Monir Fathalla <monir.fathalla@hotmail.com>
2024-05-19 20:47:46 -0700

    fix deploy

--- focusdriving/src/main/java/com/cmpt/focusdriving/FocusdrivingApplication.java
+++ focusdriving/src/main/java/com/cmpt/focusdriving/FocusdrivingApplication.java
@@ -3,10 +3,15 @@ package com.cmpt.focusdriving;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class FocusdrivingApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+        System.setProperty("DEFAULT_PASS", dotenv.get("DEFAULT_PASS"));
 		SpringApplication.run(FocusdrivingApplication.class, args);
 	}
 
